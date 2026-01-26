525e18367b40e6aa7ca2f0b7b0064f84ae1bfd41
Hyeri1ee <haerizian10@gmail.com>
2026-01-26 12:53:02 +0900

    chore(setting): 기본 의존성 추가, 테스트 API 추가

---
+++ src/main/java/whatsinmypack/mvp/controller/HelloController.java
@@ -0,0 +1,26 @@
+package whatsinmypack.mvp.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Tag(name = "Hello", description = "Hello World API")
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+
+    @Operation(summary = "테스트", description = "테스트 API")
+    @GetMapping
+    public ResponseEntity<Map<String, String>> dnd14_4th() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "DND 14기 4조 화이팅");
+        
+        return ResponseEntity.ok(response);
+    }
+
+}
