d6092529254f1f98f8b83d1b2ea903346ccd144c
kimD0ngjun <chickentasty0112@gmail.com>
2026-01-28 18:49:16 +0900

    feat(entity): 전역 테이블 생성 및 수정 컬럼 담당 추상 엔티티 작성

---
+++ src/main/java/whatsinmypack/mvp/domain/user/User.java
@@ -0,0 +1,8 @@
+package whatsinmypack.mvp.domain.user;
+
+import jakarta.persistence.Entity;
+import whatsinmypack.mvp.global.entity.BaseTimeEntity;
+
+@Entity
+public class User extends BaseTimeEntity {
+}
