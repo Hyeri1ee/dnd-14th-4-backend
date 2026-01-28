844a4a24c2726f6ba8a1ad060795cd294e7d1097
kimD0ngjun <chickentasty0112@gmail.com>
2026-01-28 17:49:42 +0900

    chore(package): DDD 기반 패키지 정리

--- src/main/java/whatsinmypack/mvp/global/config/SecurityConfig.java
+++ src/main/java/whatsinmypack/mvp/global/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package whatsinmypack.mvp.config;
+package whatsinmypack.mvp.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
