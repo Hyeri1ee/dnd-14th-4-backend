1f12e707464a7ac4dc88c0efd6273013e369af27
kimD0ngjun <chickentasty0112@gmail.com>
2026-01-29 16:57:35 +0900

    feat(security): 로그아웃 핸들러 세팅

---
+++ src/main/java/whatsinmypack/mvp/global/security/handler/CustomLogoutHandler.java
@@ -0,0 +1,52 @@
+package whatsinmypack.mvp.global.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Component;
+import whatsinmypack.mvp.global.security.jwt.JwtTokenProvider;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CustomLogoutHandler implements LogoutHandler {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void logout(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @Nullable Authentication authentication) {
+        log.info("로그아웃 핸들러 작동");
+        String tokenValue = request.getHeader(AUTHORIZATION_HEADER);
+
+        // 마지막까지 토큰 유효성 검증을 잊지말자:)
+        String decodedToken = URLDecoder.decode(tokenValue, StandardCharsets.UTF_8);
+        jwtTokenProvider.validateToken(decodedToken);
+
+        // sendResponseMsg 메소드로 로그아웃 응답 보내주기
+    }
+
+    private void sendResponseMsg(HttpServletResponse response, int statusCode, Object responseBody) throws IOException {
+        response.setStatus(statusCode);
+        response.setContentType("application/json;charset=UTF-8");
+        try (PrintWriter writer = response.getWriter()) {
+            writer.print(new ObjectMapper().writeValueAsString(responseBody));
+            writer.flush();
+        } catch(IOException e){
+            log.error(e.getMessage());
+        }
+    }
+}
