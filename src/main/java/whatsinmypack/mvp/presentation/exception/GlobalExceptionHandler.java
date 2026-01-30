5519f18390f333803fa03e224f8cc29f4c29597c
kimD0ngjun <chickentasty0112@gmail.com>
2026-01-30 21:06:48 +0900

    refactor(exception): 예외 처리기 불필요 객체 래핑 걷어냄

--- src/main/java/whatsinmypack/mvp/presentation/exception/GlobalExceptionHandler.java
+++ src/main/java/whatsinmypack/mvp/presentation/exception/GlobalExceptionHandler.java
@@ -6,9 +6,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import whatsinmypack.mvp.presentation.response.ApiResponse;
 
@@ -17,8 +18,9 @@ import whatsinmypack.mvp.presentation.response.ApiResponse;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse> handleValidationExceptions(MethodArgumentNotValidException e) {
+    public ApiResponse handleValidationExceptions(MethodArgumentNotValidException e) {
         Map<String, List<String>> fieldErrors = new HashMap<>();
 
         e.getBindingResult()
@@ -32,6 +34,6 @@ public class GlobalExceptionHandler {
                 .findFirst()
                 .orElse("잘못된 요청입니다");
 
-        return ResponseEntity.badRequest().body(new ApiResponse(message));
+        return new ApiResponse(message);
     }
 }
