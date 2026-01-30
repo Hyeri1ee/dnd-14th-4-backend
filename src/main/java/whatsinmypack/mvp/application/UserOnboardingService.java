b17085a4cdba309fa3eb0415bee946dabea5a2af
kimD0ngjun <chickentasty0112@gmail.com>
2026-01-30 23:22:49 +0900

    feat(controller): 닉네임 생성 API 생성 및 엔드포인트 할당

--- src/main/java/whatsinmypack/mvp/application/UserOnboardingService.java
+++ src/main/java/whatsinmypack/mvp/application/UserOnboardingService.java
@@ -1,6 +1,5 @@
 package whatsinmypack.mvp.application;
 
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,11 +15,12 @@ public class UserOnboardingService {
 
     private final UserRepository userRepository;
 
-    public void verifyNicknameDuplication(String nickname, String email) {
+    public void verifyAndUpdateNickname(String nickname, String email) {
         if (userRepository.existsByNickname(nickname))
             throw new DuplicateNicknameException(nickname, "이미 사용 중인 닉네임입니다");
 
         userRepository.findByEmail(email)
-                .orElseThrow(() -> new NotFoundEntityException(User.class, "해당 사용자 DB 탐색 문제 발생"));
+                .orElseThrow(() -> new NotFoundEntityException(User.class, "해당 사용자 DB 탐색 문제 발생"))
+                .updateNickname(nickname);
     }
 }
--- src/main/java/whatsinmypack/mvp/presentation/controller/UserOnboardingController.java
+++ src/main/java/whatsinmypack/mvp/presentation/controller/UserOnboardingController.java
@@ -2,21 +2,32 @@ package whatsinmypack.mvp.presentation.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import whatsinmypack.mvp.application.UserOnboardingService;
 import whatsinmypack.mvp.presentation.request.NicknameRequest;
 import whatsinmypack.mvp.presentation.response.ApiResponse;
 
 @Tag(name = "Onboarding", description = "사용자 온보딩 과정의 요청, 응답 API")
 @RestController
 @RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserOnboardingController {
 
-    @GetMapping("/nickname")
+    private final UserOnboardingService userOnboardingService;
+
+    @PostMapping("/nickname")
     @Operation(summary = "사용자 닉네임 생성", description = "사용자 닉네임 유효성 및 중복 검증 API")
-    public ApiResponse createUserNickname(@RequestBody NicknameRequest request) {
+    public ApiResponse createUserNickname(
+            @RequestBody NicknameRequest request,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        userOnboardingService.verifyAndUpdateNickname(request.getNickname(), userDetails.getUsername());
         return new ApiResponse("닉네임이 생성됐습니다");
     }
 }
