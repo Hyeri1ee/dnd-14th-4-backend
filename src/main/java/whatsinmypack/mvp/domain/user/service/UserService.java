9a74800c95abdf05369c4ebfe38fb99d0dc4f4fc
kimD0ngjun <chickentasty0112@gmail.com>
2026-02-05 23:56:44 +0900

    feat(user): 사용자 서비스 탈퇴 컨트롤러 등록

--- src/main/java/whatsinmypack/mvp/domain/user/service/UserService.java
+++ src/main/java/whatsinmypack/mvp/domain/user/service/UserService.java
@@ -3,11 +3,24 @@ package whatsinmypack.mvp.domain.user.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import whatsinmypack.mvp.domain.user.entity.User;
 import whatsinmypack.mvp.domain.user.repository.UserRepository;
+import whatsinmypack.mvp.global.security.service.KakaoApiService;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class UserService {
+
     private final UserRepository userRepository;
+    private final KakaoApiService kakaoApiService;
+
+    public void deleteUser(String username) {
+        Long kakaoId = userRepository.findByEmail(username)
+                .orElseThrow(() -> new RuntimeException("사용자 이메일이 조회되지 않음"))
+                .getKakaoId();
+
+        kakaoApiService.unlink(kakaoId);
+        userRepository.deleteByEmail(username);
+    }
 }
