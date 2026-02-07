27e387769f5959cabbf10b59a802ecad5d8b8032
Hyeri1ee <haerizian10@gmail.com>
2026-02-07 20:04:29 +0900

    feat: ItemImage value 추가

--- src/main/java/whatsinmypack/mvp/domain/item/entity/Item.java
+++ src/main/java/whatsinmypack/mvp/domain/item/entity/Item.java
@@ -5,8 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import whatsinmypack.mvp.domain.item.entity.value.ItemImage;
 import whatsinmypack.mvp.domain.user.entity.User;
 import whatsinmypack.mvp.global.entity.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "items")
@@ -39,5 +43,17 @@ public class Item extends BaseEntity {
     @JoinColumn(name = "user_id")
     private User user;//user_id FK
 
+
+    //ItemImage 고아 객체 함께 사라짐 정의
+    @OneToMany(mappedBy = "item",
+            cascade = CascadeType.ALL, //부모 영속성 작업 자식에 전파
+            orphanRemoval = true) //부모 제거시 DB에서도 자식 삭제
+    private List<ItemImage> images = new ArrayList<>();
+
+    //ItemImage 편의 메서드 : item에 item_image 추가
+    public void addImage(ItemImage image) {
+        images.add(image);
+        image.setItem(this);
+    }
 
 }
---
+++ src/main/java/whatsinmypack/mvp/domain/item/entity/value/ItemImage.java
@@ -0,0 +1,31 @@
+package whatsinmypack.mvp.domain.item.entity.value;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import whatsinmypack.mvp.domain.item.entity.Item;
+import whatsinmypack.mvp.global.entity.BaseEntity;
+
+@Entity
+@Table(name = "item_images")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ItemImage extends BaseEntity {
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String path;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    //Image 편의 메서드
+    public void setItem(Item item) {
+        this.item = item;
+    }
+}
