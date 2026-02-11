bf81a9730eb7c4eb1e9113e9f690ce930e7a2a70
Hyeri1ee <haerizian10@gmail.com>
2026-02-11 21:08:22 +0900

    chore: 주석 제거

--- src/main/java/whatsinmypack/mvp/domain/item/port/ItemPersistencePort.java
+++ src/main/java/whatsinmypack/mvp/domain/item/port/ItemPersistencePort.java
@@ -2,10 +2,7 @@ package whatsinmypack.mvp.domain.item.port;
 
 import whatsinmypack.mvp.domain.item.entity.Item;
 
-/**
- * Item 도메인 영속성 포트 (클린 아키텍처 - 아웃바운드)
- * Use Case는 이 인터페이스에만 의존하고, 구현은 Adapter가 담당한다.
- */
+
 public interface ItemPersistencePort {
 
     Item save(Item item);
