81c3ff830fb67b80f08b6f62dfe765740b1433a4
kimD0ngjun <chickentasty0112@gmail.com>
2026-02-24 22:42:21 +0900

    refactor(npe): NPE 기준 변경

--- src/main/java/whatsinmypack/mvp/adapter/in/web/pack/res/PackRecommendationResponse.java
+++ src/main/java/whatsinmypack/mvp/adapter/in/web/pack/res/PackRecommendationResponse.java
@@ -2,7 +2,9 @@ package whatsinmypack.mvp.adapter.in.web.pack.res;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.Objects;
+import whatsinmypack.mvp.domain.item.entity.value.ItemImage;
 import whatsinmypack.mvp.domain.pack.entity.Pack;
+import whatsinmypack.mvp.domain.relation.entity.PackItem;
 
 @Schema(description = "팩 추천 응답 DTO")
 public record PackRecommendationResponse(
@@ -44,17 +46,20 @@ public record PackRecommendationResponse(
         String imageUrl // 대표 이미지이미지
 ) {
     public static PackRecommendationResponse from(Pack pack) {
+        String imageUrl = pack.getPackItems().stream()
+                .findFirst()
+                .map(PackItem::getItem)
+                .flatMap(item -> item.getImages().stream().findFirst())
+                .map(ItemImage::getPath)
+                .orElse("이미지 경로가 없습니다");
+
         return new PackRecommendationResponse(
                 pack.getId(),
                 pack.getTitle(),
                 pack.getContextCategory().getName(),
                 pack.getUser().getNickname(),
                 pack.getPackItems().size(),
-                Objects.requireNonNull(Objects.requireNonNull(pack.getPackItems().stream()
-                                .findFirst()
-                                .orElse(null))
-                        .getItem().getImages().stream()
-                        .findFirst().orElse(null)).getPath()
+                imageUrl
         );
     }
 }
