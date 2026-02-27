f604d7cd7c3cc992744753da70e6b644485de42f
Hyeri1ee <haerizian10@gmail.com>
2026-02-28 08:19:37 +0900

    refactor: 팩 단건 조회, 팩 추천, 팩 검색 api에 대해서 위시리스트 여부 boolean 필드 추가

--- src/main/java/whatsinmypack/mvp/adapter/out/persistence/relation/ItemWishListJpaRepository.java
+++ src/main/java/whatsinmypack/mvp/adapter/out/persistence/relation/ItemWishListJpaRepository.java
@@ -1,6 +1,8 @@
 package whatsinmypack.mvp.adapter.out.persistence.relation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import whatsinmypack.mvp.domain.relation.entity.ItemWishList;
 
 import java.util.List;
@@ -15,4 +17,16 @@ public interface ItemWishListJpaRepository extends JpaRepository<ItemWishList, L
     void deleteByUser_Id(Long userId);
 
     void deleteByItem_Id(Long itemId);
+
+    @Query("""
+        select iw.item.id
+        from ItemWishList iw
+        where iw.user.id = :userId
+          and iw.isWishlist = true
+          and iw.item.id in :itemIds
+    """)
+    List<Long> findWishlistedItemIdsByUserIdAndItemIds(
+            @Param("userId") Long userId,
+            @Param("itemIds") List<Long> itemIds
+    );
 }
