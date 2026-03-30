c30087e583738fea09383119ce09bb19a03d81c7
Hyeri1ee <haerizian10@gmail.com>
2026-03-30 20:58:44 +0900

    chore: 테스트 환경 제작

---
+++ src/test/java/whatsinmypack/mvp/adapter/in/web/pack/PackControllerTest.java
@@ -0,0 +1,14 @@
+package whatsinmypack.mvp.adapter.in.web.pack;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PackControllerTest {
+
+    @Test
+    @DisplayName("PackControllerTest smoke")
+    void getPack() {
+        assertEquals("test", "test");
+    }
+}
